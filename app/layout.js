4cad0a3727afe69d52823d66b0a2928333957c98
Yvan Blanchette <yvanblanchette@gmail.com>
2026-07-18 23:55:43 -0400

    Suppress hydration warning noise from browser extensions on body
    Extensions like ColorZilla inject attributes (cz-shortcut-listen)
    into <body> before React hydrates, which React otherwise flags as a
    mismatch even though it's harmless and outside our control.

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
           {children}
         </ThemeProvider>
